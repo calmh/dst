b631ab08cccf9f401ec8fcb442d02e7b257fc4a7
Jakob Borg <jakob@nym.se>
2014-11-12 15:22:52 +0100

    Copyrightery

--- conn_test.go
+++ conn_test.go
@@ -1,3 +1,7 @@
+// Copyright (C) 2014 Jakob Borg and Contributors (see the CONTRIBUTORS file).
+// All rights reserved. Use of this source code is governed by an MIT-style
+// license that can be found in the LICENSE file.
+
 package dst
 
 import (
@@ -228,10 +232,6 @@ func TestLargeData(t *testing.T) {
 }
 
 func TestLargeDataLossy(t *testing.T) {
-	if testing.Short() {
-		t.Skip("slow test")
-	}
-
 	a, b, err := connPair(0.1, 0)
 	if err != nil {
 		t.Fatal(err)
--- doc.go
+++ doc.go
@@ -1,3 +1,7 @@
+// Copyright (C) 2014 Jakob Borg and Contributors (see the CONTRIBUTORS file).
+// All rights reserved. Use of this source code is governed by an MIT-style
+// license that can be found in the LICENSE file.
+
 /*
 
 Package dst implements the Datagram Stream Transfer protocol.
