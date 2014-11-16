0651720aa8d93c26b45ee0fb75aa9b509fbc3d01
Jakob Borg <jakob@nym.se>
2014-11-16 21:00:18 +0100

    Copyright stuff

--- integration_test.go
+++ integration_test.go
@@ -1,5 +1,5 @@
-// Copyright (C) 2014 Jakob Borg and Contributors (see the CONTRIBUTORS file).
-// All rights reserved. Use of this source code is governed by an MIT-style
+// Copyright 2014 The DST Authors. All rights reserved.
+// Use of this source code is governed by an MIT-style
 // license that can be found in the LICENSE file.
 
 // +build integration
